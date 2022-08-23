36f75e0e7657db147345ad7d375d90c26a0c94f3
Aleksandr Mikhailov <dmitrijdmitriev@gmail.com>
2022-08-23 21:16:09 +0300

    add commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int num1 = 150;
-        int num2 = 5000;
+        int num2 = 20;
         if (num2 >= 1000) {
             System.out.println(num1 + num2 / 100 + num2);
+            System.out.println("рублей на счету с учетом бонусов");
         } else if (num2 < 1000) {
             System.out.println(num1 + num2);
+            System.out.println("рублей на счету");
         }
     }
 }
