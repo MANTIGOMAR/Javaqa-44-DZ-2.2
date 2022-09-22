ceb840a2876aac3bce7e6334f52d6a1b8f9fa82f
Aleksandr Mikhailov <dmitrijdmitriev@gmail.com>
2022-09-22 21:39:04 +0300

    modified commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int check = 150;
-        int enroll = 999;
-        if (enroll >= 1000) {
-            System.out.println(check + enroll / 100 + enroll);
-            System.out.println("рублей на счету с учетом бонусов");
-        } else if (enroll < 1000) {
-            System.out.println(check + enroll);
-            System.out.println("рублей на счету");
+        int enroll = 1002;
+        if (enroll > 1000) {
+            System.out.println(check + enroll / 100 + enroll + "рублей еа счете," );
+            System.out.println("начисленных бонусов  " + enroll / 100);
+        } else {
+            System.out.println(check + enroll + "рублей на счете, начисленных бонусов  0");
         }
     }
 }
